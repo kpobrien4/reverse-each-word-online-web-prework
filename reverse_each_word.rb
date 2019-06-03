c33756e738db6977b8adf6cf96f5ea0c734c2763
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-03 00:29:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(string)
-  string.split(",")
-  string.each.reverse!
-  string.join
+def reverse_each_word(sentence)
+  sentence.split(",")
+  sentence.each.reverse!
+  sentence.join
 end
