1b49f7257eab70cdde4701e5e98d176b9d691ab1
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-03 00:30:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,9 +8,9 @@ def reverse_each_word_with_each(string)
 end
 
 def reverse_each_word(string)
-  array = string.split(" ") 
+  array = string.split(" ")
   test_array = []
-  array.collect do|string| 
+  array.collect do|string|
     test_array << string.reverse 
   end
   test_array.join(" ")
